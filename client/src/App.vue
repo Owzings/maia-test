deefa3a7347650266beef066409c514dc2ad57c4
ali <aliesskouri@gmail.com>
2020-08-21 11:35:45 +0200

    display usable products

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,6 +2,9 @@
   <v-app>
     <div class="d-flex justify-center">
       <h1 id="addProduct">Add Product</h1>
+          <div class="d-flex justify-center">
+             <h2 v-if="!isHiddenText">Product was added</h2>
+          </div>
     </div>
     <div class="d-flex justify-center">
       <v-col cols="6" style="margin: 0px auto;">
@@ -11,20 +14,47 @@
       </v-col>
     </div>
     <div class="d-flex justify-center">
-      <v-btn @click="addProduct()" color="primary">Add Product</v-btn>
+      <v-btn @click="addProduct(); isHiddenText = false;" color="primary">Add Product</v-btn>
+      <v-btn @click="created(); isHiddenVue = false;" color="primary">Display usable products</v-btn>
     </div>
+
+    <div v-if="!isHiddenVue" class="d-flex justify-center">
+  <h1>Usable products</h1>
+</div>
+<div v-for="product in usableProducts" :key="product._id">
+  <v-card class="mx-auto" color="white" dark max-width="800">
+    <v-card-text class="font-weight-bold title blue--text">
+    {{ product.name }} 
+      <v-list-item id="product-list-item" class="grow">
+        <v-btn @click="editProduct(product._id)" class="mx-2" small
+        color="green"> Edit </v-btn>
+       <v-btn @click="deleteProduct(product._id)" class="mx-2" small 
+       color="red"> Delete </v-btn>
+      </v-list-item>
+    </v-card-text>
+  </v-card>
+</div>
+
   </v-app>
 </template>
 <script>
 import axios from "axios";
 export default {
   data: () => ({
+    isHiddenText: true, 
+    isHiddenVue: true,
+    usableProducts: [],
     name: "",
     ean: "",
     quantity: "",
   }),
   methods: {
-    addProduct() {
+  created() {
+    axios.get("http://localhost:3000/productUsable")
+    .then(response => (this.usableProducts = response.data))
+    .catch(error => console.log(error));
+    },
+  addProduct() {
       axios.post("http://localhost:3000/product/add", {
         name: this.name,
         ean: this.ean,
